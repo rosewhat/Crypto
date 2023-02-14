a03b75bcb0a1371f361b58d238bfc17fd1a30ef2
rosewhat <whatcodingrose@gmail.com>
2023-02-14 20:13:11 +0300

    Adding database

---
+++ app/src/main/java/com/rosewhat/crypto/data/database/AppDatabase.kt
@@ -0,0 +1,38 @@
+package com.rosewhat.crypto.data.database
+
+import android.app.Application
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.rosewhat.crypto.data.pojo.CoinPriceInfo
+
+@Database(entities = [CoinPriceInfo::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+
+
+    companion object {
+        private var INSTANCE: AppDatabase? = null
+        private val LOCK = Any()
+        private const val DB_NAME = "crypto_item.db"
+
+        fun getInstance(application: Application): AppDatabase {
+            INSTANCE?.let {
+                return it
+            }
+            synchronized(LOCK) {
+                INSTANCE?.let {
+                    return it
+                }
+            }
+            val db = Room.databaseBuilder(
+                application,
+                AppDatabase::class.java,
+                DB_NAME
+            ).build()
+            INSTANCE = db
+            return db
+        }
+    }
+
+    abstract fun coinPriceInfoDao(): CoinPriceInfoDao
+}
